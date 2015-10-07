fb3b5d3e3ba6e6f3e8f5b71146a3e67f229a6db4
sanjeevm0 <sanjeevm@microsoft.com>
2015-10-07 12:33:38 -0700

    back to tp

--- src/tools/tools/basenetwork.fs
+++ src/tools/tools/basenetwork.fs
@@ -1038,8 +1038,8 @@ type [<AllowNullLiteral>] Component<'T when 'T:null and 'T:equality>() =
                                   (infoFunc : 'TP -> string) : unit =
         proc <- Component.Process item x.Dequeue x.Proc x.IsClosed x.Close tpKey infoFunc x
         compBase.SharedStateObj <- SharedComponentState.Add(compBase.ComponentId, compBase, threadPool)
-        Component<'T>.StartOnSystemThreadPool proc None
-        //Component<'T>.StartOnThreadPool threadPool proc cts tpKey infoFunc
+        //Component<'T>.StartOnSystemThreadPool proc None
+        Component<'T>.StartOnThreadPool threadPool proc cts tpKey infoFunc
         //Component<'T>.StartProcessOnOwnThread proc tpKey finishCb infoFunc
         //Prajna.Tools.ThreadPool.Current.AddWorkItem(proc, None, infoFunc(tpKey))
 
@@ -1054,8 +1054,8 @@ type [<AllowNullLiteral>] Component<'T when 'T:null and 'T:equality>() =
                                         (infoFunc : 'TP -> string) =
         let compBase = new ComponentBase()
         compBase.SharedStateObj <- SharedComponentState.Add(compBase.ComponentId, compBase, threadPool)
-        Component<'T>.StartOnSystemThreadPool func None
-        //Component<'T>.StartOnThreadPool threadPool func cts tpKey infoFunc
+        //Component<'T>.StartOnSystemThreadPool func None
+        Component<'T>.StartOnThreadPool threadPool func cts tpKey infoFunc
         //Component<'T>.StartProcessOnOwnThread func tpKey finishCb infoFunc
         //Prajna.Tools.ThreadPool.Current.AddWorkItem(func, None, infoFunc(tpKey))
         compBase
