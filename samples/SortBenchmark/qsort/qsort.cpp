3041e610fe5ab950bc4fd489a6d4235cd5f44483
sanjeevm0 <sanjeevm@microsoft.com>
2015-10-21 16:24:15 -0700

    sort benchmark

--- samples/SortBenchmark/qsort/qsort.cpp
+++ samples/SortBenchmark/qsort/qsort.cpp
@@ -230,4 +230,25 @@ void MyMemoryCopy(char* srcBuf, int srcOff, char* dest, int destOff, int dstSize
 	memcpy_s(dest + destOff, dstSize, srcBuf + srcOff, size);
 }
 
+int __cdecl compare64(void *context, const void *a, const void *b)
+{
+    int i;
+    int dim = *(int*)context;
+    unsigned __int64 *a64 = (unsigned __int64*)a;
+    unsigned __int64 *b64 = (unsigned __int64*)b;
 
+    if (a64[0] > b64[0]) return 1;
+    if (a64[0] < b64[0]) return -1;
+    for (i=1; i < dim; i++)
+    {
+        if (a64[i] > b64[i]) return 1;
+        if (a64[i] < b64[i]) return -1;
+    }
+    return 0;
+}
+
+// "buf" has "num" alignment units of size "align"
+void alignsort64(unsigned __int64 *buf, int align, int num)
+{
+    qsort_s(buf, num, align*8, compare64, &align);
+}
