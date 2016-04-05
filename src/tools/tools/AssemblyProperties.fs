088e6a8a61e4ca88b2cc1271886e926c3dd5a391
sanjeevm0 <sanjeevm@stanfordalumni.org>
2016-04-05 16:09:26 -0700

    change comment

--- src/tools/tools/AssemblyProperties.fs
+++ src/tools/tools/AssemblyProperties.fs
@@ -60,7 +60,7 @@ module internal AssemblyProperties =
     [<assembly: InternalsVisibleTo("PrajnaCopy" )>]
     // Temporary! SortBenchmark is currently using internal stuff
     [<assembly: InternalsVisibleTo("SortBenchmark")>]
-    // Temporary! SortBenchmark is currently using internal stuff
+    // Temporary! BinSort is currently using internal stuff
     [<assembly: InternalsVisibleTo("BinSort")>]
 
     do()
